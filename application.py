0771e63b09c60621b1231596e9baeffa06cf0a36
fast40 <elijahcfast@gmail.com>
2022-05-31 22:34:48 -0700

    run app

--- application.py
+++ application.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 application = Flask(__name__)
 
+
 @application.route('/')
 def index():
     return 'Hello AWS world!'
+
+
+if __name__ == "__main__":
+    application.run()
