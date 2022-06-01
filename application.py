38810832c39163aa812718a4358d01e1224cb834
fast40 <elijahcfast@gmail.com>
2022-06-01 11:05:29 -0700

    return errors

--- application.py
+++ application.py
@@ -34,25 +34,25 @@ def get_secret():
         print('\n\n-----')
     except ClientError as e:
         if e.response['Error']['Code'] == 'DecryptionFailureException':
+            return 'DecryptionFailureException'
             # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
             # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
         elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            return 'InternalServiceErrorException'
             # An error occurred on the server side.
             # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
         elif e.response['Error']['Code'] == 'InvalidParameterException':
+            return 'InvalidParameterException'
             # You provided an invalid value for a parameter.
             # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
         elif e.response['Error']['Code'] == 'InvalidRequestException':
+            return 'InvalidRequestException'
             # You provided a parameter value that is not valid for the current state of the resource.
             # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
         elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            return 'ResourceNotFoundException'
             # We can't find the resource that you asked for.
             # Deal with the exception here, and/or rethrow at your discretion.
-            raise e
     else:
         # Decrypts secret using the associated KMS key.
         # Depending on whether the secret is a string or binary, one of these fields will be populated.
